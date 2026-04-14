1c3e33621864013410cf51b0c31ee35a9847cd6e
annanagy323 <yzr25@txstate.edu>
2026-04-13 21:50:25 -0400

    First Publish Attempt
    This is just the uploaded powerpoint to make sure this works.

---
+++ Digital_Trail_Field_Guide Test/pools/387/387.js
@@ -0,0 +1,17 @@
+window.CPPool387Init = function() { cp.model['387Data']={
+};cp.poolSlideResources['pool387']={
+};
+cp.model.data['387']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["387Images"]=[];
+cp.poolResources["387Videos"]=[
+];
+cp.poolResources["387SlideVideos"]=[
+];
+}
